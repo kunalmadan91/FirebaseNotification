95c77d365e280a2d9491cf4f2016898b92659ec9
Kunal Madan <kunalmadan91@gmail.com>
2017-01-04 00:22:55 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kunal/firebasenotification/util/NotificationUtils.java
@@ -0,0 +1,140 @@
+package com.example.kunal.firebasenotification.util;
+
+import android.app.ActivityManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.text.TextUtils;
+import android.util.Patterns;
+
+import com.example.kunal.firebasenotification.R;
+import com.example.kunal.firebasenotification.app.Config;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by KUNAL on 03-01-2017.
+ */
+
+public class NotificationUtils {
+
+    private static String TAG = NotificationUtils.class.getSimpleName();
+
+    private Context mContext;
+
+    public NotificationUtils(Context mContext) {
+        this.mContext = mContext;
+    }
+
+    public void showNotificationMessage(String title, String message, String timeStamp, Intent intent) {
+        showNotificationMessage(title, message, timeStamp, intent, null);
+    }
+
+    public void showNotificationMessage(final String title, final String message, final String timeStamp, Intent intent, String imageUrl) {
+        // Check for empty push message
+        if (TextUtils.isEmpty(message))
+            return;
+
+
+        // notification icon
+        final int icon = R.mipmap.ic_launcher;
+
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        final PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(
+                        mContext,
+                        0,
+                        intent,
+                        PendingIntent.FLAG_CANCEL_CURRENT
+                );
+
+        final NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(
+                mContext);
+
+
+
+        if (!TextUtils.isEmpty(imageUrl)) {
+
+            if (imageUrl != null && imageUrl.length() > 4 && Patterns.WEB_URL.matcher(imageUrl).matches()) {
+
+
+                showSmallNotification(mBuilder, icon, title, message, timeStamp, resultPendingIntent);
+              //  Bitmap bitmap = getBitmapFromURL(imageUrl);
+
+
+                /*if (bitmap != null) {
+                    showBigNotification(bitmap, mBuilder, icon, title, message, timeStamp, resultPendingIntent, alarmSound);
+                } */
+            }
+        }
+    }
+
+
+    private void showSmallNotification(NotificationCompat.Builder mBuilder, int icon, String title, String message, String timeStamp, PendingIntent resultPendingIntent) {
+
+        NotificationCompat.InboxStyle inboxStyle = new NotificationCompat.InboxStyle();
+
+        inboxStyle.addLine(message);
+
+        Notification notification;
+        notification = mBuilder.setSmallIcon(icon).setTicker(title).setWhen(0)
+                .setAutoCancel(true)
+                .setContentTitle(title)
+                .setContentIntent(resultPendingIntent)
+                .setStyle(inboxStyle)
+                .setWhen(getTimeMilliSec(timeStamp))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(mContext.getResources(), icon))
+                .setContentText(message)
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager) mContext.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(Config.NOTIFICATION_ID, notification);
+    }
+
+    public static long getTimeMilliSec(String timeStamp) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            Date date = format.parse(timeStamp);
+            return date.getTime();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+
+    public static boolean isAppIsInBackground(Context context) {
+        boolean isInBackground = true;
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.KITKAT_WATCH) {
+            List<ActivityManager.RunningAppProcessInfo> runningProcesses = am.getRunningAppProcesses();
+            for (ActivityManager.RunningAppProcessInfo processInfo : runningProcesses) {
+                if (processInfo.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+                    for (String activeProcess : processInfo.pkgList) {
+                        if (activeProcess.equals(context.getPackageName())) {
+                            isInBackground = false;
+                        }
+                    }
+                }
+            }
+        } else {
+            List<ActivityManager.RunningTaskInfo> taskInfo = am.getRunningTasks(1);
+            ComponentName componentInfo = taskInfo.get(0).topActivity;
+            if (componentInfo.getPackageName().equals(context.getPackageName())) {
+                isInBackground = false;
+            }
+        }
+
+        return isInBackground;
+    }
+}
